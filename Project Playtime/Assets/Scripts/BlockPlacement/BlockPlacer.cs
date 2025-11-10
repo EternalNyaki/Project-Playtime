083737a42224fbdd2f06564bcc5e02942fd0634f
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2025-11-10 13:20:20 -0500

    Block placement w/ distance limit

--- Project Playtime/Assets/Scripts/BlockPlacement/BlockPlacer.cs
+++ Project Playtime/Assets/Scripts/BlockPlacement/BlockPlacer.cs
@@ -1,30 +1,56 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BlockPlacer : MonoBehaviour
 {
-    public Grid grid;
+    public SpriteRenderer placementCursor;
+    public Color validColor, invalidColor;
 
-    public Transform placementCursor;
+    public Tile tile;
 
     //How far away from the player blocks can be placed (in grid cells)
     public int placementDistance = 3;
 
+    private Tilemap m_tilemap;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (grid == null)
+        //Get tilemap for player block placement
+        foreach (var map in FindObjectsByType<Tilemap>(FindObjectsSortMode.None))
         {
-            grid = FindAnyObjectByType<Grid>();
+            if (map.tag == "PlayerTilemap")
+            {
+                m_tilemap = map;
+                break;
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get selected grid cell
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3Int cell = grid.WorldToCell(mouseWorldPos);
+        Vector3Int cell = m_tilemap.WorldToCell(mouseWorldPos);
 
-        placementCursor.position = grid.GetCellCenterWorld(cell);
-        placementCursor.localScale = grid.cellSize;
+        //Set cursor position
+        placementCursor.transform.position = m_tilemap.GetCellCenterWorld(cell);
+        placementCursor.transform.localScale = m_tilemap.cellSize;
+
+        Vector3Int playerCell = m_tilemap.WorldToCell(transform.position);
+        if (Mathf.Abs(playerCell.x - cell.x) <= placementDistance && Mathf.Abs(playerCell.y - cell.y) <= placementDistance)
+        {
+            placementCursor.color = validColor;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_tilemap.SetTile(cell, tile);
+            }
+        }
+        else
+        {
+            placementCursor.color = invalidColor;
+        }
     }
 }
